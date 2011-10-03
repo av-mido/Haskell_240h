1ee18766a650b7fd2496c757660bd5c77eb07221
Alex Vasilyev <aovasilyev@gmail.com>
2011-10-02 22:00:06 -0700

    removed old test code

--- src/Main.hs
+++ src/Main.hs
@@ -35,19 +35,16 @@ removeOuterPunctuation :: String -> String
 removeOuterPunctuation = reverse .
                           dropWhile (not . isAlpha) . reverse .
                           dropWhile (not . isAlpha)
-tRem2 = removeOuterPunctuation "--don't++"
 
 
 
 countWords :: [String] -> M.Map String Integer
 countWords =
   foldl' (\acc wrd -> M.insertWith' (+) wrd 1 acc) M.empty
-tCounts = countWords ["abc", "abc", "def", "abc", "Def", "Def", "long", "looooooooooooong"]
 
 sortWords :: M.Map String Integer -> [(String, Integer)]
 sortWords counts =
     L.sortBy (flip compare `on` snd) $ M.toList counts
-tSortedList = sortWords tCounts
 
 scaleCounts :: [(String, Integer)] -> CountsInfo
 scaleCounts xs =
@@ -57,7 +54,6 @@ scaleCounts xs =
       CountsInfo
         (L.map (\x -> (fst x, scaleCount (snd x) maxCount maxWordLength)) xs)
         maxWordLength
-tScaleCounts = scaleCounts tSortedList
 
 --not sure how to avoid using fromIntegral...
 scaleCount :: Integer -> Integer -> Integer -> Integer
@@ -72,7 +68,6 @@ printChart info =
     let xs = countsSorted info
     in
        mapM_ (printOneBar $ maxWordLength info) xs
-tPrintChart = printChart tScaleCounts
 
 printOneBar :: Integer -> (String, Integer) -> IO ()
 printOneBar maxWrdLen x =
